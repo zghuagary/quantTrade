1267d0dc6e6950a93e9a5790c45b239eabbce39f
der33425 <der33425@gmail.com>
2026-02-27 12:34:01 +0800

    獲取帳戶資金、與過往交易歷史

--- data.py
+++ data.py
@@ -1,9 +1,11 @@
-import MetaTrader5 as mt5
 import pandas as pd
-from datetime import datetime
+import MetaTrader5 as mt5
 
 if not mt5.initialize():
     print("初始化失敗")
     mt5.shutdown()
     quit()
-
+print("Mt5初始化成功")
+account_money = mt5.account_info().balance
+history = mt5.copy_rates_from_pos("USDJPY",mt5.TIMEFRAME_H1,0,1000)
+df = pd.DataFrame(history)
